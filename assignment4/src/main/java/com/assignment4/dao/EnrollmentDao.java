a40a048c2c32820b5d4f1238804a3cc0702d0db1
thishanjay <thishanishere@gmail.com>
2026-04-24 19:55:16 +0530

    Initial Spring MVC setup

--- assignment4/src/main/java/com/assignment4/dao/EnrollmentDao.java
+++
@@ -1,6 +0,0 @@
-package com.assignment4.dao;
-
-public interface EnrollmentDao {
-    boolean isAlreadyRegistered(int studentId, int courseId);
-    void registerStudent(int studentId, int courseId);
-}
